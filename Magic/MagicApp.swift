59a573258fcbf2bd1e7492165064a64362753334
Sara <sdiazper.arq@gmail.com>
2022-07-15 18:35:14 +0200

    Initial Commit

---
+++ Magic/MagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicApp.swift
+//  Magic
+//
+//  Created by Sara Diaz Perez on 15/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
